4dc51a498b9734023c71c825d077abb87ab8b9a3
JaziraU <zhurmatbekova@gmail.com>
2021-03-16 20:16:07 -0500

    Changes

--- src/test/java/MasterBranch.java
+++ src/test/java/MasterBranch.java
@@ -2,7 +2,8 @@ public class MasterBranch {
 
     public static void main(String[] args) {
         System.out.println("Master Branch");
-         System.out.println("Zhazira ");
+        System.out.println("Zhazira");
+
         
     }
 }
